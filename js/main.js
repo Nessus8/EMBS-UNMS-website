fca09debb8ef0bebdb9f23b19c06b61b8bdcce60
Nessus8 <92136016+Nessus8@users.noreply.github.com>
2021-10-18 20:22:11 -0500

    Maqueta del capitulo de ingenieria biomedica de la UNMSM

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const fila = document.querySelector('.contenedor-carousel');
+const peliculas = document.querySelector('.pelicula')
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+flechaDerecha.addEventListener('click', () => { 
+fila.scrollLeft += fila.offsetWidth;
+});
